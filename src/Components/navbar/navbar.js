831ab05cab1dbe86f66948ecb28a0b187d482b6c
Josue Resilien <joshrez20@gmail.com>
2019-10-30 00:46:27 -0400

    mobile nav

--- src/Components/navbar/navbar.js
+++ src/Components/navbar/navbar.js
@@ -10,34 +10,44 @@ export default function NavBar(props){
         color:`${`rgb(${rgb[0]},${rgb[1]},${rgb[2]})`}`
     };
     return(
-        <header class="row">
-            <nav class="col-12">
-                <div class="nav-left col-1">
-                    <img class="logo" src={adidasLogo} alt="adidas logo"></img>
-                </div>
-                <div class="col-3 spacer">
-                </div>
-                <div class="col-4 nav-center-parent">
-                    <ul class="">
-                        <li class="nav-center-child">
-                            <a class="menu-item" href="#">SHOP</a>
-                        </li>
-                        <li class="nav-center-child">
-                            <a class="menu-item" href="#">COLLECTION</a>
-                        </li>
-                        <li class="nav-center-child">
-                            <a class="menu-item" href="#">ABOUT</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-2 spacer">
-                </div>
-                
+        <header>
+            <div class="row">
+                <nav class="col-12">
+                    <div class="nav-left col-1">
+                        <img class="logo" src={adidasLogo} alt="adidas logo"></img>
+                    </div>
+                    <div class="col-3 spacer">
+                    </div>
+                    <div class="col-4 nav-center-parent">
+                        <a href="#nav-list" id="main-menu-close" class="menu-close" aria-label="Close main menu">
+                            <span class="sr-only">Close main menu</span>
+                            <span class="fa fa-close fa-3x" aria-hidden="true"></span>
+                        </a>
+                        <a href="#main-menu" class="menu-toggle" aria-label="Open main menu">
+                            <span class="fa fa-bars fa-3x" aria-hidden="true" ></span>
+                        </a> 
+                        <ul class="nav-list">
 
-                <div class="nav-right col-2">
-                    <button class="btn" id="adidas-url-btn" style={styleColor}>BACK TO ADIDAS.COM</button>
-                </div>
-            </nav>
+                            <li class="nav-center-child">
+                                <a class="menu-item" href="#">SHOP</a>
+                            </li>
+                            <li class="nav-center-child">
+                                <a class="menu-item" href="#">COLLECTION</a>
+                            </li>
+                            <li class="nav-center-child">
+                                <a class="menu-item" href="#">ABOUT</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-2 spacer">
+                    </div>
+                    
+
+                    <div class="nav-right col-2">
+                        <button class="btn" id="adidas-url-btn" style={styleColor}>BACK TO ADIDAS.COM</button>
+                    </div>
+                </nav>
+            </div>
         </header>
     );
 } 
